ab448cc45c4f6803da8de9bea241f206c025307d
RobertoHerreraCH <171G0689@rcarbonifera.tecnm.mx>
2020-10-06 21:06:51 -0500

    Actividad 1

---
+++ Actividad 1 Paso de parámetros/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Actividad_1_Paso_de_parámetros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad_1_Paso_de_parámetros.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Capturar()
+        {
+            return View();
+        }
+
+        public IActionResult ObtenerResultadoSuma(Valores a)
+        {
+            var resultado = a.Valor1 + a.Valor2;
+            return View(resultado);
+        }
+    }
+}
---
+++ Actividad 1 Paso de parámetros/Models/Valores.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actividad_1_Paso_de_parámetros.Models
+{
+    public class Valores
+    {
+       public int Valor1 { get; set; }
+       public int Valor2 { get; set; }
+    }
+}
