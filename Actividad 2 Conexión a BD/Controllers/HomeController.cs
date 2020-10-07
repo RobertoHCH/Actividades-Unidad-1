69fcc29312780bf5e010152d40e369b8e2e6d565
RobertoHerreraCH <171G0689@rcarbonifera.tecnm.mx>
2020-10-06 21:08:40 -0500

    Actividad 2

---
+++ Actividad 2 Conexión a BD/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Actividad_2_Conexión_a_BD.Models;
+
+namespace Actividad_2_Conexión_a_BD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            mexicoContext context = new mexicoContext();
+            var estados = context.Estados.OrderBy(x => x.Nombre).ToList();
+            return View(estados);
+        }
+    }
+}
---
+++ Actividad 2 Conexión a BD/Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad_2_Conexión_a_BD.Models
+{
+    public partial class Estados
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Abrev { get; set; }
+    }
+}
